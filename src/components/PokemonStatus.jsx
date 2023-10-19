1c6d0e330aeb9c479ea5716ee1c5b908c3f493f7
fdemarque <demarque.ads@gmail.com>
2023-10-18 23:42:43 -0300

    tratativa dos status dos pokemons  para filtrar e traduzir

--- src/components/PokemonStatus.jsx
+++ src/components/PokemonStatus.jsx
@@ -3,14 +3,35 @@ const PokemonStatus = (props) => {
         <div>
             <p>Status:</p>
             <ul>
-                {props.stats && (
-                    props.stats.map((e) => (
-                        <li key={e?.stat.name}>
-                            {e?.stat.name + ": " + e?.base_stat}
-                        </li>
-                    ))
-                )
-                }
+                {props?.stats &&
+                    props.stats.map((e) => {
+                        const statName = e?.stat.name;
+                        let displayName = statName;
+
+                        switch (statName) {
+                            case "attack":
+                                displayName = "ataque";
+                                break;
+                            case "defense":
+                                displayName = "defesa";
+                                break;
+                            case "speed":
+                                displayName = "velocidade";
+                                break;
+                            default:
+                                break;
+                        }
+
+                        if (["hp", "attack", "defense", "speed"].includes(statName)) {
+                            return (
+                                <li key={statName}>
+                                    {displayName + ": " + e.base_stat}
+                                </li>
+                            );
+                        }
+
+                        return null;
+                    })}
             </ul>
         </div>
     )
