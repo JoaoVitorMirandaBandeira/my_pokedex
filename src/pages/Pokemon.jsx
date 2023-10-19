8794ed032f7db1b0111d72bbdcddf4e2f21fc503
fdemarque <demarque.ads@gmail.com>
2023-10-18 22:59:48 -0300

    adição de lista com nome  habilidades do pokemon selecionado

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -7,18 +7,37 @@ const Pokemon = (props) => {
     useEffect(() => {
         const getDataPokemons = async () => {
             const pokemon = await getSpecidicPokemon(`https://pokeapi.co/api/v2/pokemon/${props.idPokemon}/`)
+            console.log(pokemon);
             setPokemon(pokemon)
 
         };
        getDataPokemons();
     },[props.idPokemon])
-    return(
+    return (
         <>
+          <div>
             <div>
-                {JSON.stringify(pokemon)}
+              <div>
+                <p>"nome":</p>
+                <p>{pokemon?.name}</p>
+              </div>
+              <div>
+                <p>"habilidades":</p>
+                <ul>
+                  {pokemon?.abilities && (
+                    pokemon.abilities.map((e) => (
+                      <li key={e?.ability.name}>
+                        {e?.ability.name}
+                      </li>
+                    ))
+                  )
+                }
+                </ul>
+              </div>
             </div>
+          </div>
         </>
-    )
+      );      
 }
 
 export  { Pokemon }
